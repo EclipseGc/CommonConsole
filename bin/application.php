425abf198f18d39236d07706cd3be821bd5e7189
Peter Pajor <peter.pajor@cheppers.com>
2020-04-20 11:45:30 +0200

    Add platform alias as optional argument.

--- bin/application.php
+++ bin/application.php
@@ -1,8 +1,13 @@
 <?php
 
+use Symfony\Component\Console\Input\InputArgument;
+
 /** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
 $container = include __DIR__ .'/../includes/container.php';
 /** @var \Symfony\Component\Console\Application $application */
 $application = $container->get('common_console_application');
 $application->setDispatcher($container->get('event_dispatcher'));
+$definition = $application->getDefinition();
+// Provide the option for specifying platform aliases.
+$definition->addArgument(new InputArgument('alias', InputArgument::OPTIONAL, 'Provide a platform alias for remote execution.'));
 $application->run($container->get('console.input'), $container->get('console.output'));
