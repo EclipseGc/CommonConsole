2b68a37513fc7ca7235ce5ca4be622d1c3bc6e1e
Peter Pajor <peter.pajor@cheppers.com>
2020-06-02 16:59:06 +0200

    Add new commands, list and describe.

---
+++ src/Command/PlatformDescribe.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace EclipseGc\CommonConsole\Command;
+
+use EclipseGc\CommonConsole\Exception\MissingPlatformException;
+use EclipseGc\CommonConsole\Platform\PlatformStorage;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class PlatformDescribe.
+ *
+ * @package EclipseGc\CommonConsole\Command
+ */
+class PlatformDescribe extends Command {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $defaultName = 'platform:describe';
+
+  /**
+   * The platform storage.
+   *
+   * @var \EclipseGc\CommonConsole\Platform\PlatformStorage
+   */
+  protected $platformStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this->setDescription('Obtain more details about a platform.')
+      ->setAliases(['pd']);
+  }
+
+  /**
+   * PlatformDescribe constructor.
+   *
+   * @param \EclipseGc\CommonConsole\Platform\PlatformStorage $storage
+   *   The platform storage service.
+   * @param string|null $name
+   *   The name of the command.
+   */
+  public function __construct(PlatformStorage $storage, string $name = NULL) {
+    parent::__construct($name);
+
+    $this->platformStorage = $storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initialize(InputInterface $input, OutputInterface $output) {
+    $alias = $input->getArgument('alias');
+    if (!$alias) {
+      throw new \Exception('Command requires the "alias" argument!');
+    }
+
+    if (!$this->platformStorage->exists($alias)) {
+      throw new MissingPlatformException($alias);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $alias = $input->getArgument('alias');
+    $platform = $this->platformStorage->load($alias);
+
+    $output->writeln(Yaml::dump($platform->export(), 7));
+  }
+
+}
