162b380dcc23f355f0bd45fd2e741f04d7fb884b
Kris Vanderwater <kris@meridian-ds.com>
2020-06-05 11:34:59 -0500

    Added ddev platform support

--- src/EventSubscriber/Platform/PlatformSubscriberDefaults.php
+++ src/EventSubscriber/Platform/PlatformSubscriberDefaults.php
@@ -5,10 +5,11 @@ namespace EclipseGc\CommonConsole\EventSubscriber\Platform;
 use EclipseGc\CommonConsole\CommonConsoleEvents;
 use EclipseGc\CommonConsole\Event\GetPlatformTypeEvent;
 use EclipseGc\CommonConsole\Event\GetPlatformTypesEvent;
+use EclipseGc\CommonConsole\Platform\DdevPlatform;
 use EclipseGc\CommonConsole\Platform\SshPlatform;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class PlatformSubscriberSsh implements EventSubscriberInterface {
+class PlatformSubscriberDefaults implements EventSubscriberInterface {
 
   public static function getSubscribedEvents() {
     $events[CommonConsoleEvents::GET_PLATFORM_TYPES] = 'onGetPlatformTypes';
@@ -18,12 +19,19 @@ class PlatformSubscriberSsh implements EventSubscriberInterface {
 
   public function onGetPlatformTypes(GetPlatformTypesEvent $event) {
     $event->addPlatformType(SshPlatform::getPlatformId());
+    $event->addPlatformType(DdevPlatform::getPlatformId());
   }
 
   public function onGetPlatformType(GetPlatformTypeEvent $event) {
     if ($event->getPlatformType() === SshPlatform::getPlatformId()) {
       $event->addClass(SshPlatform::class);
       $event->stopPropagation();
+      return;
+    }
+    if ($event->getPlatformType() === DdevPlatform::getPlatformId()) {
+      $event->addClass(DdevPlatform::class);
+      $event->stopPropagation();
+      return;
     }
   }
 
---
+++ src/Platform/DdevPlatform.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace EclipseGc\CommonConsole\Platform;
+
+use Consolidation\Config\Config;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Process\Process;
+
+/**
+ * Class DdevPlatform
+ *
+ * @package EclipseGc\CommonConsole\Platform
+ */
+class DdevPlatform extends PlatformBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getPlatformId(): string {
+    return 'DDEV';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getQuestions() {
+    return [
+      'ddev.local.wrapper_dir' => new Question("Directory containing the .ddev directory: "),
+      'ddev.remote.vendor_dir' => new Question("The location of the vendor dir within the container: "),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(Command $command, InputInterface $input, OutputInterface $output) : void {
+    $process = Process::fromShellCommandline("cd {$this->get('ddev.local.wrapper_dir')}; ddev exec {$this->get('ddev.remote.vendor_dir')}/bin/commoncli {$input->__toString()}");
+    $this->runner->run($process, $this, $output);
+  }
+
+}
