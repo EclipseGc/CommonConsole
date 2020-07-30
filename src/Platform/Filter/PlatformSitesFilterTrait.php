3010f90a6ff781cdccaa808a14c97eb75a75d188
Peter Pajor <peter.pajor@cheppers.com>
2020-07-30 16:24:10 +0200

    Move getPlatformSites into the method, make sure it exists.

--- src/Platform/Filter/PlatformSitesFilterTrait.php
+++ src/Platform/Filter/PlatformSitesFilterTrait.php
@@ -2,14 +2,15 @@
 
 namespace EclipseGc\CommonConsole\Platform\Filter;
 
+use EclipseGc\CommonConsole\Platform\PlatformSitesInterface;
 use Symfony\Component\Console\Command\Command;
 
 /**
  * Trait PlatformSitesFilterTrait
- * 
+ *
  * The trait is supposed to provide a helper method so platform could implement
  * filtering.
- * 
+ *
  * @see \EclipseGc\CommonConsole\Platform\Filter\PlatformSitesFilterInterface
  *
  * @package EclipseGc\CommonConsole\Platform\Filter
@@ -18,16 +19,24 @@ trait PlatformSitesFilterTrait {
 
   /**
    * Apply filter in case the command implements the necessary interface.
-   * 
+   *
    * @param \Symfony\Component\Console\Command\Command $command
    *   The command implementing filter mechanism.
-   * @param $sites
-   *   Available sites to filter.
+   *
+   * @return array
+   *   Filtered sites if it was applied any.
    */
-  public function applyFiltering(Command $command, &$sites): void {
+  public function getFilteredSites(Command $command): array {
+    if (!$this instanceof PlatformSitesInterface) {
+      throw new \LogicException('The platform must implement PlatformSitesInterface');
+    }
+
+    $sites = $this->getPlatformSites();
     if ($command instanceof PlatformSitesFilterInterface) {
       $command->filter($sites);
     }
+
+    return $sites;
   }
-  
+
 }
