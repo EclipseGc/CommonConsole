3c3868eb55016bd737d00aa117e625bb3a066b11
Narendra Desai <shenvidesainarendra@gmail.com>
2022-02-22 20:39:16 +0530

    LCH-5879: Added timeout option.

---
+++ src/EventSubscriber/InputDefinition/TimeoutOption.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EclipseGc\CommonConsole\EventSubscriber\InputDefinition;
+
+use EclipseGc\CommonConsole\CommonConsoleEvents;
+use EclipseGc\CommonConsole\Event\CreateApplicationEvent;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class TimeoutOption
+ *
+ * @package EclipseGc\CommonConsole\EventSubscriber\InputDefinition
+ */
+class TimeoutOption implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[CommonConsoleEvents::CREATE_APPLICATION] = 'onCreateApplication';
+    return $events;
+  }
+
+  /**
+   * Add a timeout option.
+   *
+   * @param \EclipseGc\CommonConsole\Event\CreateApplicationEvent $event
+   *   The create input event.
+   */
+  public function onCreateApplication(CreateApplicationEvent $event) {
+    // Provide the option for specifying timeout.
+    $event->getApplication()->getDefinition()->addOption(new InputOption('timeout', NULL, InputOption::VALUE_REQUIRED, 'The process timeout in seconds.'));
+  }
+
+}
